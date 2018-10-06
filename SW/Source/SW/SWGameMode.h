fcb24207d475bdb974449cd74368cde3c7d1f78a
swonlee <twiclok@naver.com>
2018-10-06 21:46:21 +0900

    Initial Commit UE4 SWProject.

---
+++ SW/Source/SW/SWGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SWGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASWGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASWGameMode();
+};
+
+
+
